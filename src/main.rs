88b48d6b8eac702bcc0f8d05452cb6fb767b6e4e
Florian Kraft <schnuffifk@gmail.com>
2024-04-14 18:42:20 +0200

    responds with echo

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,26 @@ use std::net::TcpListener;
 #[derive(Debug)]
 struct HttpRequest {
     path: String,
-    method: String,
+    method: HttpMethod,
     version: String,
 }
+
+impl HttpRequest {
+    fn new(path: &str, version: &str, method: &str) -> Self {
+        let http_method = match method {
+            "GET" => HttpMethod::GET,
+            _ => todo!("implement later"),
+        };
+        Self {
+            path: path.to_string(),
+            method: http_method,
+            version: version.to_string(),
+        }
+    }
+}
+
 const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
+const CONTENT_TYPE: &str = "Content-Type: text/plain\r\n\r\n";
 const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const ERROR_RESPONSE: &str = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
 
@@ -19,30 +35,50 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 let mut buffer = [0; 1024];
-                match stream.read(&mut buffer) {
+                let result = match stream.read(&mut buffer) {
                     Ok(_) => {
                         let req = std::str::from_utf8(&buffer).unwrap();
                         let http_request = parse_req(req).unwrap();
-                        let mut resp = NOT_FOUND_RESPONSE;
-                        println!("path: {:?}", http_request.path);
-                        if http_request.path == "/" {
-                            resp = OK_RESPONSE;
-                        }
+                        let resp = handle_request(&http_request);
+                        stream.write(resp.as_bytes())
+                    }
+                    Err(_) => stream.write(ERROR_RESPONSE.as_bytes()),
+                };
 
-                        match stream.write(resp.as_bytes()) {
-                            Ok(_) => println!("OK"),
-                            Err(e) => println!("Error! ({:?})", e),
-                        }
-                    }
-                    Err(_) => {
-                        stream.write(ERROR_RESPONSE.as_bytes()).unwrap();
-                    }
+                match result {
+                    Ok(_) => println!("ok"),
+                    Err(e) => println!("error: {:?}", e),
                 }
             }
+
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+#[derive(Debug)]
+enum HttpMethod {
+    GET,
+}
+
+fn echo_response(path: &str) -> String {
+    let values: Vec<&str> = path.split("/").collect();
+    println!("{:?}", values);
+    let body = values[2];
+    let length = body.len();
+    let content_length = format!("Content-Length: {}", length);
+    format!("{}{}{}\r\n{}", OK_RESPONSE, CONTENT_TYPE, content_length, body)
+}
+
+fn handle_request(http_request: &HttpRequest) -> String {
+    let req_path = http_request.path.as_str();
+    match http_request.method {
+        HttpMethod::GET => match req_path {
+            "/" => OK_RESPONSE.to_string(),
+            path if path.starts_with("/echo") => echo_response(path),
+            _ => NOT_FOUND_RESPONSE.to_string(),
+        },
     }
 }
 
@@ -50,11 +86,11 @@ fn parse_req(req: &str) -> Result<HttpRequest, Error> {
     let contents: Vec<&str> = req.lines().collect();
     println!("contents: {:?}", contents);
     let mut method_header = contents[0].split_whitespace();
-    let http_request = HttpRequest {
-        method: String::from(method_header.next().unwrap()),
-        path: String::from(method_header.next().unwrap()),
-        version: String::from(method_header.next().unwrap()),
-    };
+    // TODO clean this up omg - this depends on the field order at the moment
+    let method = method_header.next().unwrap();
+    let path = method_header.next().unwrap();
+    let version = method_header.next().unwrap();
+    let http_request = HttpRequest::new(&path, &version, &method);
     println!("http req: {:?}", http_request);
     Ok(http_request)
 }
